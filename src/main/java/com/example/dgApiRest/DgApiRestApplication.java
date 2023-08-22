873e4c4e073571cb1da3f8c1ef7cd04b9f7f02f5
Nestor Nieri <nraulnieri@gmail.com>
2023-08-22 01:04:50 -0500

    Inicializando proyecto api rest para datagrowth

---
+++ src/main/java/com/example/dgApiRest/DgApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dgApiRest/DgApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
